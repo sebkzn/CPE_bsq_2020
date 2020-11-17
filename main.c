84c9015b0fc84904bdf02bd57285e7dd4d3bcf43
Sebastien Raoult <sebastien.raoult@epitech.eu>
2020-11-17 12:35:46 +0100

    open a map and read into a buffer

--- main.c
+++ main.c
@@ -5,9 +5,42 @@
 ** main
 */
 
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include "my.h"
+
+static char *fs_open_file(char const *filepath)
+{
+    struct stat info;
+    int fd;
+    char *buffer;
+
+    if (stat(filepath, &info) == -1 || !(info.st_size))
+        return (NULL);
+    fd = open(filepath, O_RDONLY);
+    if (fd == -1)
+        return (NULL);
+    buffer = malloc(sizeof(char) * info.st_size + 1);
+    if (!buffer)
+        return (NULL);
+    if (read(fd, buffer, info.st_size) != info.st_size)
+        return (NULL);
+    buffer[info.st_size] = '\0';
+    close(fd);
+    return (buffer);
+}
 
 int main(int argc, char * const *argv)
 {
+    char *buffer = NULL;
+
+    if (argc != 2)
+        return (84);
+    buffer = fs_open_file(argv[1]);
+    if (buffer == NULL)
+        return (84);
+    my_putstr(buffer);
+    free(buffer);
     return (0);
 }
